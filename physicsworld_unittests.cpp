f1d3d56c0c3948afd2b7df4c77fed824973503f5
Phil Hyatt <phyatt16@studentbody.byu.edu>
2018-12-06 15:26:55 -0700

    Started adding a physics robot to unit tests

--- physicsworld_unittests.cpp
+++ physicsworld_unittests.cpp
@@ -57,11 +57,11 @@ TEST(RoboticsUnitTest,WhenPuttingJointsBetweenLinks_TheyReturnRelativeTransformB
     PhysicsCylinder *cylinder1 = new PhysicsCylinder;
     cylinder1->height = 1.5;
     cylinder1->radius = .1;
+    world.add_object_to_world(cylinder1);
+
     PhysicsCylinder *cylinder2 = new PhysicsCylinder;
     cylinder2->height = 1.5;
     cylinder2->radius = .1;
-
-    world.add_object_to_world(cylinder1);
     world.add_object_to_world(cylinder2);
 
     PhysicsJoint * joint1 = new PhysicsJoint;
@@ -70,6 +70,71 @@ TEST(RoboticsUnitTest,WhenPuttingJointsBetweenLinks_TheyReturnRelativeTransformB
     joint1->TransformFromParentCoMToJoint = Eigen::Translation3d(0,0,1);
     joint1->rotationAxis << 0, 0, 1;
     joint1->TransformFromJointToChildCoM = Eigen::Translation3d(0,1,0) * Eigen::AngleAxisd(3.14159,Eigen::Vector3d(0,0,1));
+
+
+    Eigen::Affine3d ExpectedTransformation = Eigen::Translation3d(0,1,1) * Eigen::AngleAxisd(3.14159,Eigen::Vector3d(0,0,1));
+
+    Eigen::Affine3d actualTransformation = joint1->get_transform_from_parent_CoM_to_child_CoM();
+
+    EXPECT_EQ(ExpectedTransformation(0,0),actualTransformation(0,0));
+    EXPECT_EQ(ExpectedTransformation(1,0),actualTransformation(1,0));
+    EXPECT_EQ(ExpectedTransformation(2,0),actualTransformation(2,0));
+    EXPECT_EQ(ExpectedTransformation(3,0),actualTransformation(3,0));
+    EXPECT_EQ(ExpectedTransformation(0,1),actualTransformation(0,1));
+    EXPECT_EQ(ExpectedTransformation(1,1),actualTransformation(1,1));
+    EXPECT_EQ(ExpectedTransformation(2,1),actualTransformation(2,1));
+    EXPECT_EQ(ExpectedTransformation(3,1),actualTransformation(3,1));
+    EXPECT_EQ(ExpectedTransformation(0,2),actualTransformation(0,2));
+    EXPECT_EQ(ExpectedTransformation(1,2),actualTransformation(1,2));
+    EXPECT_EQ(ExpectedTransformation(2,2),actualTransformation(2,2));
+    EXPECT_EQ(ExpectedTransformation(3,2),actualTransformation(3,2));
+    EXPECT_EQ(ExpectedTransformation(0,3),actualTransformation(0,3));
+    EXPECT_EQ(ExpectedTransformation(1,3),actualTransformation(1,3));
+    EXPECT_EQ(ExpectedTransformation(2,3),actualTransformation(2,3));
+    EXPECT_EQ(ExpectedTransformation(3,3),actualTransformation(3,3));
+
+}
+
+TEST(RoboticsUnitTest,WhenCreatingRobotAndGivingJointAngles_RobotCanDoForwardKinematics)
+{
+    PhysicsWorld world;
+
+    PhysicsRobot robot;
+
+    PhysicsBox *box = new PhysicsBox;
+    box->height = 1.0;
+    box->length = 1.0;
+    box->width = 1.0;
+
+    robot->base = box;
+
+    PhysicsCylinder *cylinder1 = new PhysicsCylinder;
+    cylinder1->height = 1.0;
+    cylinder1->radius = .1;
+    world.add_object_to_world(cylinder1);
+
+    PhysicsJoint * joint1 = new PhysicsJoint;
+    joint1->parent = robot.base;
+    joint1->child = cylinder1;
+    joint1->TransformFromParentCoMToJoint = Eigen::Translation3d(0,robot->width/2.0,);
+    joint1->rotationAxis << 1, 0, 0;
+    joint1->TransformFromJointToChildCoM = Eigen::Translation3d(0,cylinder1->height/2.0,0) * Eigen::AngleAxisd(0,Eigen::Vector3d(0,0,1));
+
+    robot.add_joint(joint1);
+
+    PhysicsCylinder *cylinder2 = new PhysicsCylinder;
+    cylinder2->height = 1.0;
+    cylinder2->radius = .1;
+    world.add_object_to_world(cylinder2);
+
+    PhysicsJoint * joint2 = new PhysicsJoint;
+    joint1->parent = cylinder1;
+    joint1->child = cylinder2;
+    joint1->TransformFromParentCoMToJoint = Eigen::Translation3d(0,cylinder1->height/2.0,0);
+    joint1->rotationAxis << 1, 0, 0;
+    joint1->TransformFromJointToChildCoM = Eigen::Translation3d(0,cylinder2->height/2.0,0) * Eigen::AngleAxisd(0,Eigen::Vector3d(0,0,1));
+
+    Eigen::Affine3d robotEEPose = robot.get_pose_of_link_end(1);
 
 }
 
